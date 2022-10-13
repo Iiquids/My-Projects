2e897d19054c9f3bf498e3602b064ad585bc33e1
Iiquids <83255875+Iiquids@users.noreply.github.com>
2022-10-13 12:23:27 -0400

    Create Key.py

---
+++ Key.py
@@ -0,0 +1,15 @@
+""" _______________________
+【   】【   】【   】【   】【   】
+【   】【   】【   】【   】【   】
+【   】【   】【   】【   】【   】
+【   】【   】【   】【   】【   】
+【   】【   】【   】【   】【   】
+【   】【   】【   】【   】【   】
+ ----------------------- """
+ 
+ #Table
+ 
+ #Change String Color
+ 
+print("\033[1;32m This text is Bright Green  \n")
+print("\u001b[33m This text is Yellow \n")
