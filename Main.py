8dcfe530e29ee8dbbfab819b2fddfc67d7dac61d
Iiquids <83255875+Iiquids@users.noreply.github.com>
2022-10-16 20:03:15 -0400

    Wordle

--- Main.py
+++ Main.py
@@ -1,97 +1,134 @@
-from random import choice
+import time
+from Words import getRandomWord
 
-possibleWords = ["Abuse", "Adult", "Agent", "Anger", "Apple", "Award", "Basis", "Beach", "Birth", "Block", "Blood", "Board", "Brain", "Bread", "Break", "Brown", "Buyer", "Cause", "Chain", "Chair", "Chest", "Chief", "Child", "China", "Claim", "Class", "Clock", "Coach", "Coast", "Court", "Cover", "Cream", "Crime", "Cross", "Crowd", "Crown", "Cycle", "Dance", "Death", "Depth", "Doubt", "Draft", "Drama", "Dream", "Dress", "Drink", "Drive", "Earth", "Enemy", "Entry", "Error", "Event", "Faith", "Fault", "Field", "Fight", "Final", "Floor", "Focus", "Force", "Frame", "Frank", "Front", "Fruit", "Glass", "Grant", "Grass", "Green", "Group", "Guide", "Heart", "Henry", "Horse", "Hotel", "House", "Image", "Index", "Input", "Issue", "Japan", "Jones", "Judge", "Knife", "Laura", "Layer", "Level", "Lewis", "Light", "Limit", "Lunch", "Major", "March", "Match", "Metal", "Model", "Money", "Month", "Motor", "Mouth", "Music", "Night", "Noise", "North", "Novel", "Nurse", "Offer", "Order", "Other", "Owner", "Panel", "Paper", "Party", "Peace" ]
+'''
+This project is gonna beat Edin's clapped up project
+by Brayan and Edin
+12 October 2022
+'''
 
-## Variables
-WORD = choice(possibleWords)
-table = []
-Empty = "【 】【 】【 】【 】【 】"
-Answers = []
+# all the crazy variables
+totalTries = 0
 
-for i in WORD:
-    table.append(i)
-## Variable End
+''' This is just for debug and checking if I accidentally add duplicate words
+for x in possibleWordList:
+    if possibleWordList.count(x) != 1:
+        print(possibleWordList[x]) '''
 
-## Spooky Functions
+theWord = getRandomWord()
+emptyBoxes = '[ ][ ][ ][ ][ ]'
+answerBoard = []
+fullAnswerBoard = []
+avaliableKeyboard = ''' Q W E R T Y U I O P
+  A S D F G H J K L
+   Z X C V B N M''' # something Edin never worked on
 
-def checkEqual(a, b):
-    a = a.lower()
-    b = b.lower()
-    if a == b:
+#the color codes to spice things up
+def prGreen(msg): return f"\033[1;32m{msg.upper()}\033[0m"
+def prBlack(msg): return f"\033[0;30m{msg.upper()}\033[0m"
+def prYellow(msg): return f"\u001b[33m{msg.upper()}\033[0m"
+def prPurple(msg): return f"\033[0;35m{msg.upper()}\033[0m"
+
+# now we gotta do the functions Edin told me to do
+def wordChecker(wordInput):
+    global avaliableKeyboard
+    totalRow = ''
+    for x in range(0, 5):
+        if wordInput[x] == theWord[x]:
+            greenLetter = prGreen(wordInput[x])
+            totalRow = totalRow + f'[{greenLetter}]'
+            avaliableKeyboard = avaliableKeyboard.replace(wordInput[x], greenLetter)
+        elif wordInput[x] in theWord:
+            totalRow = dupeChecker(wordInput[x], totalRow)
+        else:
+            blackLetter = prBlack(wordInput[x])
+            totalRow = totalRow + f'[{blackLetter}]'
+            avaliableKeyboard = avaliableKeyboard.replace(wordInput[x], blackLetter)
+    winCheck(totalRow)
+    fillBoard(totalRow)
+
+def dupeChecker(letter, rowIP): # Edin doesn't have a repeat function so obviously my wordle is better
+    global avaliableKeyboard
+    dupeCount = theWord.count(letter)
+    if dupeCount == 1:
+        yellowLetter = prYellow(letter)
+        totalRow = rowIP + f'[{yellowLetter}]'
+        avaliableKeyboard = avaliableKeyboard.replace(letter, yellowLetter)
+    else:
+        purpleLetter = prPurple(letter)
+        totalRow = rowIP + f'[{purpleLetter}]'
+        avaliableKeyboard = avaliableKeyboard.replace(letter, purpleLetter)
+    return totalRow
+
+def fillBoard(currentRow):
+    answerBoard.append(currentRow)
+    for a in range(0, len(answerBoard)):
+        fullAnswerBoard.append(answerBoard[a])
+    for b in range(5 - totalTries):
+        fullAnswerBoard.append(emptyBoxes)
+
+def createAnswer():
+    answer = ''
+    for x in range(0, 5):
+        greenLetter = prGreen(theWord[x])
+        answer = answer + f'[{greenLetter}]'
+    return answer
+
+def winCheck(row): # Edin isn't gonna win so I don't have to worry about this function ever being used
+    if createAnswer() in row:
+        print('\nCONGRATS YOU BEAT betterWORDLE. THIS PROJECT IS BETTER THAN EDIN\'S!')
+        fillBoard(row)
+        for num in range(len(fullAnswerBoard)):
+            print(fullAnswerBoard[num])
+        time.sleep(1)
+        exit()
+
+def vowelNumCheck(wordInput):
+    vowels = "AEIOU"
+    vowelState = False
+    for y in vowels:
+        if y in wordInput:
+            vowelState = True
+    if len(wordInput) != 5:
+        vowelState = False
+    return vowelState
+    
+def alphaCheck(inWord):
+    alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    verifier = 0
+    for alphLetter in alph:
+        for guessLetter in inWord:
+            if alphLetter == guessLetter:
+                verifier += 1
+    if verifier == 5:
         return True
     else:
         return False
 
-def turnGreen(msg):
-    return f"\033[1;32m{msg.upper()}\033[0m"
+def guess(guessWord):
+    guessWord = guessWord.upper()
+    while vowelNumCheck(guessWord) == False:
+        guessWord = input('Your word isn\'t a real world, Enter again: ')
+        guessWord = guessWord.upper()
+        vowelNumCheck(guessWord)
+    while alphaCheck(guessWord) == False:
+        guessWord = input('Your word isn\'t a real world, Enter again: ')
+        guessWord = guessWord.upper()
+        alphaCheck(guessWord)
+    wordChecker(guessWord)
+    for num in range(len(fullAnswerBoard)):
+        print(fullAnswerBoard[num]) 
+    print(f'  Avaliable letters: \n{avaliableKeyboard} \n')
 
-def turnYellow(msg):
-    return f"\u001b[33m{msg.upper()}\033[0m"
-
-def turnBlack(msg):
-    return f"\033[0;30m{msg.upper()}\033[0m"
-
-def turnRed(msg):
-    return f"\033[0;31m{msg}\033[0m"
-
-def getStringColor(a):
-    for i in range(0, len(WORD)):
-        bool = checkEqual(a, table[i])
-        if bool == True:
-            return turnYellow(a)
-    return turnBlack(a)
-
-gameRounds = 0
-def checkWord(text):
-    global gameRounds
-    final = ""
-    guessTable = []
-    Finished = 0
-    for i in text:
-        guessTable.append(i)
-
-    for i in range(0, len(WORD)):
-        bool = checkEqual(guessTable[i], table[i])
-        stringColor = getStringColor(guessTable[i])
-        if bool == True:
-            Finished = Finished + 1
-            local = guessTable[i]
-            final = final + f"【{turnGreen(local)}】"
-        else:
-            final = final + f"【{stringColor}】"
-    if Finished == 5:
-        print(turnGreen("YOU GUESSSED IT! WINNER WINNER CHICKEN DINNER!"))
-        exit()
-    gameRounds = gameRounds + 1
-    return final
-
-def guess():
-    global gameRounds
-    guess = input()
-    if len(guess) != 5:
-        while len(guess) != 5:
-            print(f"The Length Of {guess} is not 5.")
-            guess = input()
-    guess = guess.lower()
-    round = checkWord(guess)
-    Answers.append(round)
-    for i in Answers:
-        print(i)
-    for i in range(0, 6-gameRounds):
-        print(Empty)
-
-#End Of Spooky Functions
-
-print("Enter Your First Guess:")
-guess()
-print("Enter Your Secound Guess:")
-guess()
-print("Enter Your Third Guess:")
-guess()
-print("Enter Your Fourth Guess:")
-guess()
-print("Enter Your Fifth Guess:")
-guess()
-print("Enter Your Sixth Guess:")
-guess()
-print(turnRed("Welp... Out of Luck."))
+# now for the tries loop
+print('WELCOME TO betterWORDLE, THIS IS THE COMPETITION AGAINST EDIN\'S PROJECT\n') # welcome message
+while totalTries != 6:
+    fullAnswerBoard = []
+    if totalTries == 0:
+        guess(input('Enter your first 5 letter word: '))
+    elif totalTries == 5:
+        guess(input('This is your last guess, choose wisely: '))
+        print(f'The Word: {theWord}')
+    else:
+        guess(input('Enter your next guess: '))
+    totalTries += 1
